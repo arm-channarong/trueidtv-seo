fc68a21f0d20c11d1a46b68d7700696baa79525f
Ryan Douglas Archer <ryandouglasarcher@TDG-M0696.local>
2023-06-15 20:08:14 +0700

    remove index.html file

--- schema/en/trueidplus.js
+++ schema/en/trueidplus.js
@@ -1,44 +1,108 @@
-{
+[
+  {
     "@context": "http://schema.org/",
-    "@type": "ItemList",
+    "@type": "BreadcrumbList",
     "itemListElement": [
-        {
-            "@type": "Product",
-            "name": "TrueID Plus Package",
-            "image": "https://ui.dmpcdn.com/tvbox-web-cdn/imgs/channels/trueidplus-banner-horizontal.webp",
-            "description": "Watch TrueID Plus for free for 1 month. Package starts at THB59/month.",
-            "brand": {
-                "@type": "Brand",
-                "name": "TrueID TV"
-            },
-            "offers": {
-                "@type": "Offer",
-                "priceCurrency": "THB",
-                "price": "59",
-                "url": "https://myaccount.trueid.net/th/checkout?productCode=TIDPLUS&utm_source=trueidtv.trueid.net&utm_medium=page-trueidplus&utm_content=trueidplus-package-subscribe-btn&source_caller=ui&shortlink=g3zjo1ny&c=trueidtv&pid=trueid_web&af_xp=custom",
-                "availability": "https://schema.org/InStock",
-                "itemCondition": "https://schema.org/NewCondition",
-                "priceValidUntil": "2023-06-30"
-            }
-        },
-        {
-            "@type": "Product",
-            "name": "TrueID Plus Package",
-            "image": "https://ui.dmpcdn.com/tvbox-web-cdn/imgs/channels/trueidplus-banner-horizontal.webp",
-            "description": "Watch TrueID Plus for free for 1 month. Package starts at THB599/year.",
-            "brand": {
-                "@type": "Brand",
-                "name": "TrueID TV"
-            },
-            "offers": {
-                "@type": "Offer",
-                "priceCurrency": "THB",
-                "price": "599",
-                "url": "https://myaccount.trueid.net/th/checkout?productCode=TIDPLUS&utm_source=trueidtv.trueid.net&utm_medium=page-trueidplus&utm_content=trueidplus-package-subscribe-btn&source_caller=ui&shortlink=g3zjo1ny&c=trueidtv&pid=trueid_web&af_xp=custom&offer_code=TIDPLUS_BPN_OC_1Y_REG",
-                "availability": "https://schema.org/InStock",
-                "itemCondition": "https://schema.org/NewCondition",
-                "priceValidUntil": "2023-06-30"
-            }
-        }
+      {
+        "@type": "ListItem",
+        "position": 1,
+        "name": "Home",
+        "item": "https://trueidtv.trueid.net/en"
+      },
+      {
+        "@type": "ListItem",
+        "position": 2,
+        "name": "TrueID+",
+        "item": "https://trueidtv.trueid.net/en/trueidplus"
+      }
     ]
-}
+  },
+  {
+    "@context": "http://schema.org/",
+    "@type": "FAQPage",
+    "mainEntity": [
+      {
+        "@type": "Question",
+        "name": "What is TrueID+ package?",
+        "acceptedAnswer": {
+          "@type": "Answer",
+          "text": "TrueID+ package is a monthly subscription package for viewing movies online via TrueID TV box as well as TrueID application and website."
+        }
+      },
+      {
+        "@type": "Question",
+        "name": "How much is the TrueID+ package subscription?",
+        "acceptedAnswer": {
+          "@type": "Answer",
+          "text": "Currently, there's a special offer for the TrueID+ package at 59 Baht per month (was 119 Baht) from Jun 1, 2023 - Jun 30, 2023* and a yearly subscription of 599 Baht per month.\n*Existing TrueMove H customers can view some TrueID+ content without additional charges until Jun 30, 2023. Promotions are valid within the specified period."
+        }
+      },
+      {
+        "@type": "Question",
+        "name": "Can I use my TruePoint to redeem for TrueID+ package?",
+        "acceptedAnswer": {
+          "@type": "Answer",
+          "text": "No, it is not possible to use TruePoint to redeem for this package right now."
+        }
+      },
+      {
+        "@type": "Question",
+        "name": "How can I subscribe to the TrueID+ package?",
+        "acceptedAnswer": {
+          "@type": "Answer",
+          "text": "You can subscribe to TrueID+ package via TrueID TV website or TrueID application iOS/Android\n(existing TrueMove H customers can view some TrueID+ content without additional charges until Jun 30, 2023)."
+        }
+      },
+      {
+        "@type": "Question",
+        "name": "How do I pay for the TrueID+ package subscription?",
+        "acceptedAnswer": {
+          "@type": "Answer",
+          "text": "We accept the following payment for package subscription: credit card, TrueMoney Wallet, or place charges on your TrueMove H bill.\nTo subscribe for this package, please choose this package on your TV screen. Then, use your mobile phone or tablet to scan QR code displayed on the TV screen when asked for payment. After successful subscription, you will be automatically charged for package every 30 days.\nOr, you can also visit the TrueID.net website to subscribe for TrueID+ package using a credit card or TrueMoney Wallet account."
+        }
+      },
+      {
+        "@type": "Question",
+        "name": "Can I get a free trial of TrueID+ package?",
+        "acceptedAnswer": {
+          "@type": "Answer",
+          "text": "If you susbcribe to TrueID+ package via the TrueID TV box or the application, you will recI apologize for the confusion earlier. Here is the JSON-LD structured data from the TrueID+ page, including all the FAQ entries:
+
+```json
+[
+  {
+    "@context": "http://schema.org/",
+    "@type": "BreadcrumbList",
+    "itemListElement": [
+      {
+        "@type": "ListItem",
+        "position": 1,
+        "name": "Home",
+        "item": "https://trueidtv.trueid.net/en"
+      },
+      {
+        "@type": "ListItem",
+        "position": 2,
+        "name": "TrueID+",
+        "item": "https://trueidtv.trueid.net/en/trueidplus"
+      }
+    ]
+  },
+  {
+    "@context": "http://schema.org/",
+    "@type": "FAQPage",
+    "mainEntity": [
+      {
+        "@type": "Question",
+        "name": "What is TrueID+ package?",
+        "acceptedAnswer": {
+          "@type": "Answer",
+          "text": "TrueID+ package is a monthly subscription package for viewing movies online via TrueID TV box as well as TrueID application and website."
+        }
+      },
+      {
+        "@type": "Question",
+        "name": "How much is the TrueID+ package subscription?",
+        "acceptedAnswer": {
+          "@type": "Answer",
+          "text": "Currently, there's a special offer for the TrueID+ package at 59 Baht per
