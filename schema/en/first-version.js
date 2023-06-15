075eb0593ed217aa6ed530474c3a4f2290fdf9b6
Ryan Douglas Archer <ryandouglasarcher@TDG-M0696.local>
2023-06-15 17:10:17 +0700

    push structured data (EN) for specs, unlock, channels and tv app

---
+++ schema/en/first-version.js
@@ -0,0 +1,18 @@
+{
+  "@context": "https://schema.org/",
+  "@type": "BreadcrumbList",
+  "itemListElement": [
+    {
+      "@type": "ListItem",
+      "position": 1,
+      "name": "Home",
+      "item": "https://trueidtv.trueid.net/en"
+    },
+    {
+      "@type": "ListItem",
+      "position": 2,
+      "name": "TrueID TV box gen 1",
+      "item": "https://trueidtv.trueid.net/en/first-version"
+    }
+  ]
+}
---
+++ schema/en/tvapp.js
@@ -0,0 +1,51 @@
+{
+    "@context": "http://schema.org/",
+    "@graph": [
+        {
+            "@type": "BreadcrumbList",
+            "itemListElement": [
+                {
+                    "@type": "ListItem",
+                    "position": 1,
+                    "name": "Home",
+                    "item": "https://trueidtv.trueid.net/en"
+                },
+                {
+                    "@type": "ListItem",
+                    "position": 2,
+                    "name": "TrueID TV app",
+                    "item": "https://trueidtv.trueid.net/en/tvapp"
+                }
+            ]
+        },
+        {
+            "@type": "FAQPage",
+            "mainEntity": [
+                {
+                    "@type": "Question",
+                    "name": "How do I setup TrueID TV app on streaming device or smart TV?",
+                    "acceptedAnswer": {
+                        "@type": "Answer",
+                        "text": "You can download and install TrueID TV app as follows:\nFrom home of Android TV, go to 'App'\nSelect Google Play Store app\nGo to 'Search' at the top of the display and type in 'TrueID TV'\nSelect the app and select install, then follow the instructions on your screen\nHow to install the app on Android TV"
+                    }
+                },
+                {
+                    "@type": "Question",
+                    "name": "Is the TrueID TV app available on mobile phone or tablet?",
+                    "acceptedAnswer": {
+                        "@type": "Answer",
+                        "text": "There is no 'TrueID TV app' on mobile or tablet. You can watch the same list of movies, series and TV shows as well as continue to watch where you left off on the TrueID app. Simply download it from Google Play Store or App Store."
+                    }
+                },
+                {
+                    "@type": "Question",
+                    "name": "How do I solve problems related to TrueID TV app?",
+                    "acceptedAnswer": {
+                        "@type": "Answer",
+                        "text": "You can find support topics related to TrueID TV app"
+                    }
+                }
+            ]
+        }
+    ]
+}
