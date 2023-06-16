87c4b55e36317c0f3bbc6fff181e24b04e37aaef
Ryan Douglas Archer <ryandouglasarcher@TDG-M0696.local>
2023-06-16 15:27:57 +0700

    create the code for tol unlock page

---
+++ schema/en/tol-unlock.js
@@ -0,0 +1,115 @@
+{
+  "@context": "https://schema.org",
+  "@graph": [
+    {
+      "@type": "BreadcrumbList",
+      "itemListElement": [
+        {
+          "@type": "ListItem",
+          "position": 1,
+          "name": "Home",
+          "item": "https://trueidtv.trueid.net/en"
+        },
+        {
+          "@type": "ListItem",
+          "position": 2,
+          "name": "TrueUnlock TV",
+          "item": "https://trueidtv.trueid.net/en/unlocktv/new-trueonline"
+        }
+      ]
+    },
+    {
+      "@type": "Product",
+      "name": "TrueUnlock TV",
+      "image": "https://trueidtv.trueid.net/en/unlocktv/new-trueonline",
+      "description": "Get internet package 500/500 Mbps to unlock entertainment on TrueID TV box including premium channels, movies & series, and world-class sports for only ฿599",
+      "brand": {
+        "@type": "Brand",
+        "name": "TrueOnline"
+      },
+      "offers": {
+        "@type": "Offer",
+        "url": "https://trueidtv.trueid.net/en/unlocktv/new-trueonline",
+        "priceCurrency": "THB",
+        "price": "599"
+      }
+    },
+    {
+      "@type": "Product",
+      "name": "TrueUnlock TV",
+      "image": "https://trueidtv.trueid.net/en/unlocktv/new-trueonline",
+      "description": "Get internet package 1000/500 Mbps to unlock entertainment on TrueID TV box including premium channels, movies & series, and world-class sports for only ฿799",
+      "brand": {
+        "@type": "Brand",
+        "name": "TrueOnline"
+      },
+      "offers": {
+        "@type": "Offer",
+        "url": "https://trueidtv.trueid.net/en/unlocktv/new-trueonline",
+        "priceCurrency": "THB",
+        "price": "799"
+      }
+    },
+    {
+      "@type": "FAQPage",
+      "mainEntity": [
+        {
+          "@type": "Question",
+          "name": "Who can get TrueUnlock TV for free?",
+          "acceptedAnswer": {
+            "@type": "Answer",
+            "text": "TrueOnline customers who subscribe to the package of ฿599 or higher are eligible to enjoy TrueUnlock TV package for free."
+          }
+        },
+        {
+          "@type": "Question",
+          "name": "After applying for TrueOnline package, how can I get TrueUnlock TV?",
+          "acceptedAnswer": {
+            "@type": "Answer",
+            "text": "If you are using TrueOnline, you can check your eligibility and get TrueUnlock TV package to watch the content for free for a period of 24 months through this website. New customers who subscribe to a TrueOnline package starting at ฿599 will be eligible to get TrueUnlock TV and more benefits. See TrueOnline packages."
+          }
+        },
+        {
+          "@type": "Question",
+          "name": "How can I watch Premier League matches for free?",
+          "acceptedAnswer": {
+            "@type": "Answer",
+            "text": "By getting the TrueUnlock TV package, you can choose one Premier League team of your choice and enjoy live broadcasts of all matches featuring your favorite team without any additional cost throughout the package period."
+          }
+        },
+        {
+          "@type": "Question",
+          "name": "Can I watch all matches of the Premier League?",
+          "acceptedAnswer": {
+            "@type": "Answer",
+            "text": "True Unlock TV package is a special privilege for TrueOnline customers. With TrueUnlock TV package, you are able to choose 1 Premier league team of your choice and enjoy watching live matches of your favorite team for free.\n\nIf you would like to watch all Premier league matches, we recommend subscribing to our True Premier Football package. It offers daily, weekly, monthly or whole seasonal options at a very reasonable price."
+          }
+        },
+        {
+          "@type": "Question",
+          "name": "What can I watch with TrueUnlock TV?",
+          "acceptedAnswer": {
+            "@type": "Answer",
+            "text": "Other than the Premier League, you can watch other TV programs such as national or international sports matches, popular Thai or Hollywood movies as well as hit series, animations, documentaries, and game shows. Programs are available on live channels and as reruns."
+          }
+        },
+        {
+          "@type": "Question",
+          "name": "Can I watch TrueUnlock TV from different devices at the same time?",
+          "acceptedAnswer": {
+            "@type": "Answer",
+            "text": "You can watch TrueUnlock TV on 2 devices on any platform at the same time (TrueID TV, Web, mobile devices)"
+          }
+        },
+        {
+          "@type": "Question",
+          "name": "Is there any cost after TrueUnlock TV package ends?",
+          "acceptedAnswer": {
+            "@type": "Answer",
+            "text": "Is there any cost after TrueUnlock TV package ends?"
+          }
+        }
+      ]
+    }
+  ]
+}
