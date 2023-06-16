399ed071ea7b0039a9a8a84763cbb873890bb425
Ryan Douglas Archer <ryandouglasarcher@TDG-M0696.true.true>
2023-06-16 12:24:20 +0700

    add thai structured data

---
+++ schema/th/packages.js
@@ -0,0 +1,360 @@
+{
+  "@context": "https://schema.org",
+  "@graph": [
+    {
+      "@type": "BreadcrumbList",
+      "itemListElement": [
+        {
+          "@type": "ListItem",
+          "position": 1,
+          "name": "หน้าหลัก",
+          "item": "https://trueidtv.trueid.net"
+        },
+        {
+          "@type": "ListItem",
+          "position": 2,
+          "name": "แพ็กเกจ",
+          "item": "https://trueidtv.trueid.net/packages"
+        }
+      ]
+    },
+    {
+      "@type": "FAQPage",
+      "mainEntity": [
+        {
+          "@type": "Question",
+          "name": "กล่องทรูไอดี ทีวี ราคาเท่าไหร่ ซื้อได้ที่ไหน?",
+          "acceptedAnswer": {
+            "@type": "Answer",
+            "text": "ลูกค้าทั่วไปสามารถซื้อกล่องทรูไอดี ทีวี ได้ในราคา 2,490 บาท โดยสามารถสั่งซื้อออนไลน์ได้ง่ายๆ แค่คลิกที่นี่"
+          }
+        },
+        {
+          "@type": "Question",
+          "name": "กล่องทรูไอดี ทีวี มีค่าบริการรายเดือนไหม?",
+          "acceptedAnswer": {
+            "@type": "Answer",
+            "text": "กล่องทรูไอดี ทีวีมีค่าบริการรายเดือนเพียงเดือนละ 50 บาท สามารถดูช่องรายการดัง หนังดี การ์ตูนฮิต ช่องรายการโทรทัศน์และอื่นๆ ใน Basic Package (ไม่มีค่าบริการ) หรือสามารถเลือกซื้อแพ็กเกจอื่นๆ ได้ที่นี่"
+          }
+        }
+      ]
+    },
+    {
+      "@type": "Product",
+      "name": "รับฟรี กล่องทรูไอดี ทีวี เพียงสมัครเน็ตบ้านทรูวันนี้",
+      "image": "https://ui.dmpcdn.com/tvbox-web-cdn/imgs/banners/campaign-tol-internet-package-banner.webp",
+      "description": "สมัครเน็ตบ้านพร้อมใช้แพ็กเกจรายเดือน 599.- ขึ้นไป รับฟรี กล่องทรูไอดี ทีวี",
+      "brand": {
+              "@type": "Brand",
+              "name": "ทรูไอดี ทีวี"
+      },
+      "offers": {
+              "@type": "Offer",
+              "priceCurrency": "THB",
+              "price": "599",
+              "url": "https://trueidtv.trueid.net/th/packages",
+              "availability": "https://schema.org/InStock",
+              "itemCondition": "https://schema.org/NewCondition"
+      }
+    },
+    {
+      "@type": "Product",
+      "name": "รับฟรี กล่องทรูไอดี ทีวี เพียงเปิดซิมเน็ตไม่อั้น",
+      "image": "https://ui.dmpcdn.com/tvbox-web-cdn/imgs/banners/campaign-entertainment-sim-package-banner.webp",
+      "description": "เปิดซิมพร้อมใช้แพ็กเกจทรูมูฟ เอชรายเดือน 399.- ขึ้นไป รับฟรี กล่องทรูไอดี ทีวี",
+      "brand": {
+              "@type": "Brand",
+              "name": "ทรูไอดี ทีวี"
+      },
+      "offers": {
+              "@type": "Offer",
+              "priceCurrency": "THB",
+              "price": "399",
+              "url": "https://trueidtv.trueid.net/th/packages",
+              "availability": "https://schema.org/InStock",
+              "itemCondition": "https://schema.org/NewCondition"
+      }
+    },
+    {
+      "@type": "Product",
+      "name": "แพ็กเกจ ทรูไอดี พลัส รายเดือน พิเศษ 99.-",
+      "image": "https://ui.dmpcdn.com/tvbox-web-cdn/imgs/pics/trueid-tvbox-ver2.webp",
+      "description": "ลูกค้าปัจจุบัน ทรูออนไลน์ ทรูมูฟ เอช รับกล่อง ทรูไอดี ทีวี พร้อม TrueID+ เพียงเดือนละ 99 บาท",
+      "brand": {
+              "@type": "Brand",
+              "name": "ทรูไอดี ทีวี"
+      },
+      "offers": {
+              "@type": "Offer",
+              "priceCurrency": "THB",
+              "price": "99",
+              "url": "https://trueidtv.trueid.net/th/packages",
+              "availability": "https://schema.org/InStock",
+              "itemCondition": "https://schema.org/NewCondition"
+      }
+    },
+    {
+      "@type": "Product",
+      "name": "กล่องทรูไอดี ทีวี",
+      "image": "https://ui.dmpcdn.com/tvbox-web-cdn/imgs/pics/trueid-tvbox-ver2.webp",
+      "description": "กล่องทรูไอดี ทีวี ชมช่องรายการเด่นผ่าน ทรูไอดี ฟรี สั่งซื้อเพียง 2,490.-",
+      "brand": {
+              "@type": "Brand",
+              "name": "ทรูไอดี ทีวี"
+      },
+      "offers": {
+              "@type": "Offer",
+              "priceCurrency": "THB",
+              "price": "2490",
+              "url": "https://trueidtv.trueid.net/th/packages",
+              "availability": "https://schema.org/InStock",
+              "itemCondition": "https://schema.org/NewCondition"
+      }
+    },
+    {
+      "@type": "Product",
+      "name": "ทรูไอดี วัน",
+      "image": "https://ui.dmpcdn.com/tvbox-web-cdn/imgs/channels/trueidone-package-banner-horizontal-01.webp",
+      "description": "ทรูไอดี วัน แพ็กเดียวที่รวมความบันเทิงเอเชียและฮอลลีวูดจาก 4 แพลตฟอร์ม TrueID+, Prime Video, WeTV, iQIYI รายเดือน 299.-",
+      "brand": {
+              "@type": "Brand",
+              "name": "ทรูไอดี ทีวี"
+      },
+      "offers": {
+              "@type": "Offer",
+              "priceCurrency": "THB",
+              "price": "299",
+              "url": "https://trueidtv.trueid.net/th/packages",
+              "availability": "https://schema.org/InStock",
+              "itemCondition": "https://schema.org/NewCondition"
+      }
+    },
+    {
+      "@type": "Product",
+      "name": "ทรูไอดี พลัส รายเดือน ",
+      "image": "https://ui.dmpcdn.com/tvbox-web-cdn/imgs/channels/trueidplus-banner-horizontal.webp",
+      "description": "แพ็กเกจทรูไอดี พลัส ดูฟรี 30 วัน เริ่มต้นเพียงเดือนละ 59.- ไม่มีโฆษณาคั่น",
+      "brand": {
+              "@type": "Brand",
+              "name": "ทรูไอดี ทีวี"
+      },
+      "offers": {
+              "@type": "Offer",
+              "priceCurrency": "THB",
+              "price": "59",
+              "url": "https://trueidtv.trueid.net/th/packages",
+              "availability": "https://schema.org/InStock",
+              "itemCondition": "https://schema.org/NewCondition"
+      }
+    },
+    {
+      "@type": "Product",
+      "name": "ทรูพรีเมียร์ลีก",
+      "image": "https://ui.dmpcdn.com/tvbox-web-cdn/imgs/channels/epl-football-banner-horizontal-02.jpg",
+      "description": "ทรูพรีเมียร์ลีก Early Bird ดูครบทุกคู่ ซื้อก่อนคุ้มกว่า",
+      "brand": "TrueID",
+      "offers": {
+        "@type": "Offer",
+        "url": "https://ttid.co/gDfF/coipywb9",
+        "priceCurrency": "THB",
+        "price": "2900",
+        "priceValidUntil": "2023-08-09"
+      }
+    },
+    {
+      "@context": "https://schema.org/",
+      "@type": "Product",
+      "name": "ทรูวิชั่นส์ นาว ไลท์",
+      "image": "https://ui.dmpcdn.com/tvbox-web-cdn/imgs/channels/tvs-now-banner-horizontal.jpg",
+      "description": "เพิ่มความสนุกให้กับการดูทีวีจากช่องทรูวิชั่นส์ เพลินกับรายการกว่า 1,500 รายการ",
+      "brand": "TrueVisions NOW",
+      "offers": {
+        "@type": "Offer",
+        "url": "https://ttid.co/gDfF/7dvnsn8v",
+        "priceCurrency": "THB",
+        "price": "119"
+      }
+    },
+    {
+      "@type": "Product",
+      "name": "ทรูวิชั่นส์ นาว เวิลด์",
+      "image": "https://ui.dmpcdn.com/tvbox-web-cdn/imgs/channels/tvs-now-world-horizontal.webp",
+      "description": "ดูเต็มอิ่มกับช่องข่าวต่างประเทศชั้นนำ เช่น NHK World TV, CNBC, YTN, CGTN, Russia Today และอื่นๆ",
+      "brand": "TrueVisions NOW",
+      "offers": {
+        "@type": "Offer",
+        "url": "https://ttid.co/gDfF/fi5kgnle",
+        "priceCurrency": "THB",
+        "price": "119"
+      }
+    },
+    {
+      "@type": "Product",
+      "name": "ทรูวิชั่นส์ นาว สแตนดาร์ด",
+      "image": "https://ui.dmpcdn.com/tvbox-web-cdn/imgs/channels/tvs-now-standard-banner-horizontal.webp",
+      "description": "พิ่มความบันเทิงอีกขั้นกับการ์ตูน วาไรตี้ สารคดี ดูสดยูฟ่า แชมเปี้ยนส์ลีก ลาลีกา ลีกเอิง",
+      "brand": "TrueVisions NOW",
+      "offers": {
+        "@type": "Offer",
+        "url": "https://ttid.co/gDfF/8dm5xp6x",
+        "priceCurrency": "THB",
+        "price": "249"
+      }
+    },
+    {
+      "@type": "Product",
+      "name": "ทรูวิชั่นส์ นาว ไลฟ์สไตล์",
+      "image": "https://ui.dmpcdn.com/tvbox-web-cdn/imgs/channels/tvs-now-lifestyle-horizontal.webp",
+      "description": "แพ็กเกจเอาใจสายทำอาหาร และแฟชั่น ด้วยคอนเทนต์คุณภาพจากช่องทรูวิชั่นส์",
+      "brand": "TrueVisions NOW",
+      "offers": {
+        "@type": "Offer",
+        "url": "https://ttid.co/gDfF/ygy2lav6",
+        "priceCurrency": "THB",
+        "price": "399"
+      }
+    },
+    {
+      "@type": "Product",
+      "name": "กล่องทรูไอดี ทีวี พร้อมแพ็กความบันเทิง ซิม & เน็ตบ้าน",
+      "image": "https://ui.dmpcdn.com/tvbox-web-cdn/imgs/banners/campaign-true-unlock-banner.webp",
+      "description": "สมัครแพ็กเกจรวมบริการ กล่องทรูไอดี พร้อมปลดล็อกความบันเทิง ซิม & เน็ตบ้าน เพียง ฿599/เดือน",
+      "brand": {
+        "@type": "Brand",
+        "name": "True"
+      },
+      "offers": {
+        "@type": "Offer",
+        "url": "https://trueidtv.trueid.net/en/packages",
+        "priceCurrency": "THB",
+        "price": "599"
+      }
+    },
+    {
+      "@type": "Product",
+      "name": "TrueVisions NOW Family",
+      "image": "https://ui.dmpcdn.com/tvbox-web-cdn/imgs/channels/tvs-now-family-banner-horizontal.webp",
+      "description": "สนุกได้ทั้งครอบครัว สารคดี และ ไลฟ์สไตล์",
+      "brand": {
+              "@type": "Brand",
+              "name": "TrueID TV"
+      },
+      "offers": {
+              "@type": "Offer",
+              "priceCurrency": "THB",
+              "price": "399",
+              "url": "https://trueidtv.trueid.net/th/packages",
+              "availability": "https://schema.org/InStock",
+              "itemCondition": "https://schema.org/NewCondition"
+      }
+    },
+    {
+      "@type": "Product",
+      "name": "TrueVisions NOW Tennis",
+      "image": "https://ui.dmpcdn.com/tvbox-web-cdn/imgs/channels/tvs-now-tennis-banner-horizontal.webp",
+      "description": "ถ่ายทอดสดเทนนิส ครบทุกแกรนด์สแลม ชมสดทัวร์นาเมนต์มากที่สุดในไทย ที่ทรูวิชั่นส์ ที่เดียว",
+      "brand": {
+              "@type": "Brand",
+              "name": "TrueID TV"
+      },
+      "offers": {
+              "@type": "Offer",
+              "priceCurrency": "THB",
+              "price": "399",
+              "url": "https://trueidtv.trueid.net/th/packages",
+              "availability": "https://schema.org/InStock",
+              "itemCondition": "https://schema.org/NewCondition"
+      }
+    },
+    {
+      "@type": "Product",
+      "name": "TrueVisions NOW Premium",
+      "image": "https://ui.dmpcdn.com/tvbox-web-cdn/imgs/channels/tvs-now-premium-banner-horizontal.webp",
+      "description": "TrueVisions NOW PREMIUM เชียร์มันส์ จัดเต็มทุกกีฬา อัดแน่นทุกความบันเทิง หนัง การ์ตูน วาไรตี้ต่างประเทศ รวม 43 ช่อง กว่า 3,800 รายการ",
+      "brand": {
+              "@type": "Brand",
+              "name": "TrueID TV"
+      },
+      "offers": {
+              "@type": "Offer",
+              "priceCurrency": "THB",
+              "price": "449",
+              "url": "https://trueidtv.trueid.net/th/packages",
+              "availability": "https://schema.org/InStock",
+              "itemCondition": "https://schema.org/NewCondition"
+      }
+    },
+    {
+      "@type": "Product",
+      "name": "TrueVisions NOW EPL & UEFA",
+      "image": "https://ui.dmpcdn.com/tvbox-web-cdn/imgs/channels/tvs-now-epl-and-uefa-banner-horizontal.webp",
+      "description": "เชียร์บอลพรีเมียร์ลีก และบอลยุโรป จากช่อง beIN SPORTS ครบสุดใน 1 แพ็กเกจ",
+      "brand": {
+              "@type": "Brand",
+              "name": "TrueID TV"
+      },
+      "offers": {
+              "@type": "Offer",
+              "priceCurrency": "THB",
+              "price": "499",
+              "url": "https://trueidtv.trueid.net/th/packages",
+              "availability": "https://schema.org/InStock",
+              "itemCondition": "https://schema.org/NewCondition"
+      }
+    },
+    {
+      "@type": "Product",
+      "name": "ทรูวิชั่นส์ นาว กอล์ฟ",
+      "image": "https://ui.dmpcdn.com/tvbox-web-cdn/imgs/channels/tvs-now-golf-banner-horizontal.webp",
+      "description": "แพ็กเกจทรูวิชั่นส์ นาว กอล์ฟ ดูกอล์ฟ ครบ จบในแพ็กเดียว รายเดือน 629.-
+",
+      "brand": {
+              "@type": "Brand",
+              "name": "ทรูไอดี ทีวี"
+      },
+      "offers": {
+              "@type": "Offer",
+              "priceCurrency": "THB",
+              "price": "629",
+              "url": "https://trueidtv.trueid.net/th/packages",
+              "availability": "https://schema.org/InStock",
+              "itemCondition": "https://schema.org/NewCondition"
+      }
+    },
+    {
+      "@type": "Product",
+      "name": "ทรูวิชั่นส์ นาว โกลด์",
+      "image": "https://ui.dmpcdn.com/tvbox-web-cdn/imgs/channels/tvs-now-gold-banner-horizontal.webp",
+      "description": "แพ็กเกจ ทรูวิชั่นส์ นาว โกลด์ รวมช่องหนังฮิต ซีรีส์ต่างประเทศ กีฬายอดนิยม รายเดือน 999.-
+",
+      "brand": {
+              "@type": "Brand",
+              "name": "ทรูไอดี ทีวี"
+      },
+      "offers": {
+              "@type": "Offer",
+              "priceCurrency": "THB",
+              "price": "999",
+              "url": "https://trueidtv.trueid.net/th/packages",
+              "availability": "https://schema.org/InStock",
+              "itemCondition": "https://schema.org/NewCondition"
+      }
+    },
+    {
+      "@type": "Product",
+      "name": "TrueVisions NOW PLATINUM",
+      "image": "https://ui.dmpcdn.com/tvbox-web-cdn/imgs/channels/tvs-now-platinum-banner-horizontal.webp",
+      "description": "รวมที่สุดกีฬา และความบันเทิงระดับโลก เพียง 1,499 บาท/เดือน รับชมบอลสด 8 ลีก 10 ถ้วย กว่า 1,500 แมทซ์ ทั้งหนัง ซีรีส์ สารคดี การ์ตูน แชร์กันดูได้สูงสุด 4 จอ",
+      "brand": {
+        "@type": "Brand",
+        "name": "True"
+      },
+      "offers": {
+        "@type": "Offer",
+        "url": "https://trueidtv.trueid.net/th/packages",
+        "priceCurrency": "THB",
+        "price": "1499"
+      }
+    }
+  ]
+}
